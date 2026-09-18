6f69c264df9af951bcd1a8849c494df33144c1cc
emre beştaş <83371445+emreBestas@users.noreply.github.com>
2026-09-18 18:54:49 +0300

    Add project files.

---
+++ FlightBookingKrch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FlightBookingKrch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

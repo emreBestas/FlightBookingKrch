abb6e7bf68dc031c8ebb47b8beef2ffd7f2e0046
emre beştaş <83371445+emreBestas@users.noreply.github.com>
2026-09-21 17:39:17 +0300

    7 sonu

---
+++ FlightBookingKrch/ViewComponents/BookingViewComponents/_BookingTagsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookingKrch.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }   
+    }
+}
---
+++ FlightBookingKrch/ViewComponents/DefaultViewComponents/_DefaultSliderComponentPartial.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookingKrch.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultSliderComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+
+    }
+}

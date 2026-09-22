eb4899570045c060c3cf701d4a3bd594cf11292e
emre beştaş <83371445+emreBestas@users.noreply.github.com>
2026-09-22 11:02:59 +0300

    Dtos

---
+++ FlightBookingKrch/Mapping/GeneralMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using FlightBookingKrch.Dtos.FlightDtos;
+using FlightBookingKrch.Entities;
+
+namespace FlightBookingKrch.Mapping
+{
+    public class GeneralMapping: Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Flight, CreateFlightDto>().ReverseMap();
+            CreateMap<Flight, UpdateFlightDto>().ReverseMap();
+            CreateMap<Flight, GetFlightByIdDto>().ReverseMap();
+            CreateMap<Flight, ResultFlightDto>().ReverseMap();
+        }
+    }
+}
